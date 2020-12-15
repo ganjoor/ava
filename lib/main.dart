166e736f0ea8458a722cef77ded40d47ba185951
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-12-15 21:57:12 +0330

    adding Samim font to the project

--- lib/main.dart
+++ lib/main.dart
@@ -44,17 +44,17 @@ class AvaAppState extends State<AvaApp> {
     return MaterialApp(
         title: 'آوای گنجور',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+            fontFamily: 'Samim'),
         onGenerateRoute: router.generator,
         builder: (BuildContext context, Widget child) {
           return Directionality(
@@ -234,6 +234,18 @@ class _MyHomePageState extends State<MyHomePage>
                   // the App.build method, and use it to set our appbar title.
                   title: Text('آوای گنجور'),
                   actions: [
+                    IconButton(
+                      icon: Icon(Icons.mic),
+                      tooltip: 'نحوهٔ مشارکت',
+                      onPressed: () async {
+                        var url = 'http://ava.ganjoor.net/about/';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'خطا در نمایش نشانی $url';
+                        }
+                      },
+                    ),
                     IconButton(
                         icon: Icon(Icons.refresh),
                         tooltip: 'تازه‌سازی',
